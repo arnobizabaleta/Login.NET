3ceef90d0d56f4647af0b42a506cd71a8e43ef63
Arnobi Zabaleta <arnobizabaleta@gmail.com>
2022-08-20 16:49:44 -0500

    Agregar archivos de proyecto.

---
+++ Permisos/ValidarSessionAttributte.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PRUEBAS_LOGIN.Permisos
+{
+    public class ValidarSessionAttributte : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (HttpContext.Current.Session["usuario"] == null)//Si no se ha iniciado Sesión
+            {
+                filterContext.Result = new RedirectResult("~/Acceso/Login");//Redirecciona al login session
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
